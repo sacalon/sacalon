98a02ce22fde64a610e493813b973269d480d325
Bistcuite <85484922+bistcuite@users.noreply.github.com>
2021-11-04 21:02:10 +0330

    update actions

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-$python=python
-$pip=pip3
-$pyinstaller=pyinstaller
 
-$pip install -r ./requirements.txt
-$python -m $pyinstaller --noconfirm --onefile --console --name "hascal"  "src/hascal.py"
+
+pip3 install -r ./requirements.txt
+python -m pyinstaller --noconfirm --onefile --console --name "hascal"  "src/hascal.py"
 cp -r ./src/hlib ./dist/	
 cp -r ./examples/ ./dist/
