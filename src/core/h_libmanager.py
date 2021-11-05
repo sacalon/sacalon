159bb13865afe2f3af68140378c973a05e33a048
Bistcuite <85484922+bistcuite@users.noreply.github.com>
2021-11-05 21:20:36 +0330

    create hascal library manager file

---
+++ src/core/h_libmanager.py
@@ -0,0 +1,10 @@
+# The Hascal Library Manager
+#
+# The Hascal Programming Language
+# Copyright 2019-2022 Hascal Development Team,
+# all rights reserved.
+
+def install_lib(lib_name):
+    ...
+def uninstall_lib(lib_name):
+    ...
