d886c685edf0742c73f70f8e3041c56cb20b36bd
Bistcuite <85484922+bistcuite@users.noreply.github.com>
2021-11-08 18:23:58 +0330

    port os library to c++

---
+++ src/hlib/libcpp/os.cc
@@ -0,0 +1,18 @@
+string os_name()
+{
+    #ifdef _WIN32
+    return "win32";
+    #elif _WIN64
+    return "win64";
+    #elif __APPLE__ || __MACH__
+    return "macos";
+    #elif __linux__
+    return "linux";
+    #elif __FreeBSD__
+    return "freebsd";
+    #elif __unix || __unix__
+    return "unix";
+    #else
+    return "unknown";
+    #endif
+}    
