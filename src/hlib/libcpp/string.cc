932f6486b22012ce7d11fb469971d38d72941c95
Bistcuite <85484922+bistcuite@users.noreply.github.com>
2021-12-03 17:09:45 +0330

    add libcpp.string library

---
+++ src/hlib/libcpp/string.cc
@@ -0,0 +1,25 @@
+bool is_alpha(char c){
+    return std::is_alpha(c);
+}
+
+bool is_alphanum(char c){
+    return std::is_alpha(c) || std::is_digit(c);
+}
+
+string to_lower(string str){
+    string temp;
+    for (char c : str)
+    {
+        temp += std::tolower(c);
+    }
+    return temp;
+}
+
+string to_upper(string str){
+    string temp;
+    for (char c : str)
+    {
+        temp += std::toupper(c);
+    }
+    return temp;
+}
