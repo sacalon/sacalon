cf1af3101ca20cbe1ccce8af419017c4e888a448
bistcuite <85484922+bistcuite@users.noreply.github.com>
2022-03-18 17:53:16 +0330

    setup.py : fix typo error

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 print(setuptools.find_packages())
 setuptools.setup(
     name='hascal',
-    version='1.3.8-alhpa',
+    version='1.3.8-alpha',
     author='Hascal Foundation',
     description='Hascal is a general purpose and open source programming language designed to build optimal, maintainable, reliable and efficient software.',
 
