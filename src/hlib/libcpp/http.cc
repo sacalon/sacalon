699c2d079d4c6d292226d723688453ad8359d4e6
bistcuite <85484922+bistcuite@users.noreply.github.com>
2022-04-27 19:37:11 +0430

    hlib : add post function to http library

--- src/hlib/libcpp/http.cc
+++ src/hlib/libcpp/http.cc
@@ -20,12 +20,39 @@ string get(string url){
         curl_easy_cleanup(curl);
 
         if(res != CURLE_OK)
-            return "Error";
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
             
         return readBuffer + "\n";
-    }else {
-        return "Error";
     }
+    return "";
+}
+
+string post(string url,string post_data){
+    CURL *curl;
+    CURLcode res;
+    string readBuffer;
+
+    curl_global_init(CURL_GLOBAL_ALL);
+    
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_data.c_str());
+        res = curl_easy_perform(curl);
+
+        if(res != CURLE_OK)
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+    
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+    return readBuffer + "\n";
 }
 
 bool download(string url,string path){
