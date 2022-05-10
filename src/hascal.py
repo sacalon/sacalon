75ee7c906a74047209a267f5687809de1aca5250
bistcuite <85484922+bistcuite@users.noreply.github.com>
2022-05-10 11:37:38 +0430

    src : bug fix

--- src/hascal.py
+++ src/hascal.py
@@ -6,13 +6,13 @@
 # Copyright 2019-2022 Hascal Development Team,
 # all rights reserved.
 
-from sys import argv
+import sys
 from core.h_builder import HascalCompiler
 import pathlib
 
 def main():
     BASE_DIR = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-    HascalCompiler(argv, BASE_DIR)
+    HascalCompiler(sys.argv, BASE_DIR)
 
 if __name__ == "__main__":
     main()
