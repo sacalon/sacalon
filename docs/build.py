b94e118d646de797091204017c385f285b997313
bistcuite <85484922+bistcuite@users.noreply.github.com>
2022-08-08 12:32:24 +0430

    docs : add book builder script

---
+++ docs/build.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+from os.path import isdir
+
+os.system("mdbook build")
+os.system("cd stdlib & mdbook build & cd ..")
+os.system("cd book & mkdir stdlib & cd ..")
+    
+source_dir = './stdlib/book'
+target_dir = './book/stdlib'
+    
+file_names = os.listdir(source_dir)
+
+for file_name in file_names:
+    shutil.move(os.path.join(source_dir, file_name), target_dir)
