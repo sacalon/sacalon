5f26f6032046d95737e2bc86b633974257ae9097
komeil Parseh <ahmdparsh129@gmail.com>
2022-08-09 14:25:07 +0430

    chore: fix release action bug

--- .github/workflows/publish.py
+++ .github/workflows/publish.py
@@ -3,6 +3,7 @@ import os
 import sys
 from datetime import datetime
 from pathlib import Path
+from platform import system
 from subprocess import getoutput
 
 from github import Github
@@ -10,7 +11,23 @@ from github import Github
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
 api_key = os.getenv("api_key", None)
-current_date = datetime.today().strftime("%Y-%m-%d")
+release_name = os.getenv("release_name", None)
+
+if not (api_key and release_name):
+    logging.error(
+        "'api_key'/'release_name' not found in your envs."
+        f"please add this and run again, your envs are: {api_key} and {release_name}"
+    )
+    sys.exit(1)
+
+
+# build
+logging.info(msg="Building...")
+if system() == "Windows":
+    os.system("make windows")
+else:
+    os.system("make build")
+logging.info(msg="Build complete")
 
 try:
     path = list(
@@ -22,28 +39,25 @@ except IndexError:
     logging.error("No built binary found")
     sys.exit(1)
 
-repo_name = os.getenv("name", None)
-release_name = os.getenv("release_name", None)
+if system().lower().startswith("nt"):
+    name = "windows"
+elif system().lower().startswith("linux"):
+    name = "linux"
+elif system().lower().startswith("darwin"):
+    name = "macos"
+else:
+    name = system().lower()
 
-if release_name is None:
-    release_name = getoutput("git describe --tags --always")
-
-logging.info("Starting at %s", current_date)
-
-if not (repo_name and api_key and release_name):
-    logging.error(
-        "'repo_name'/'api_key'/'release_name' not found in your envs."
-        "please add this and run again"
-    )
-    sys.exit(1)
-
+path = (
+    Path(os.path.dirname(os.path.abspath(__file__)))
+    / "../../"
+    / path.rename(f"hascal-{name}-{release_name}")
+)
 
 gh = Github(api_key)
-repo = gh.get_repo(f"hascal/{repo_name}")
+repo = gh.get_repo("hascal/hascal")
 
 release = repo.get_release(release_name)
-
 logging.info("statrting Upload build file to release")
-
-release.upload_asset(path=path)
+release.upload_asset(path=str(os.path.normpath(path)))
 logging.info("upload is done")
